ead60f44e98923b5800f1bda820cf77218b3337a
satriopurbo <satrio_leonheart@yahoo.com>
2024-12-16 10:21:57 +0700

    asd

--- src/module/club/controller.js
+++ src/module/club/controller.js
@@ -17,4 +17,23 @@ export class Controller {
             res.status(500).json({ status: 500, message: "gagal", data: error })
         }
     }
+
+    static async details_by_club_id(req,res){
+        const{club_id}=req.body
+        try {
+            let data = await osbond.query(`SELECT * FROM APPS_LISTCLUB()`,tipe())
+            data = data.recordset
+            let detail=``
+            data.forEach(el => {        
+               if(el.club_id==club_id){
+                detail=el
+               }
+            });
+            res.status(200).json({ status: 200, message: "sukses" ,detail})
+        } catch (error) {
+            console.log(req.body)
+            console.log(error)
+            res.status(500).json({ status: 500, message: "gagal", data: error })
+        }
+    }
 }
--- src/module/club/route.js
+++ src/module/club/route.js
@@ -8,7 +8,7 @@ const router = Router()
 
 
 router.post('/list', Controller.list)
-
+router.post('/details_by_club_id', Controller.details_by_club_id)
 
 
 export default router
