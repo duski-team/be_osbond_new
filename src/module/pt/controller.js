d33a54c9e8da27bc1265c572b42cf7a2ed709ebe
satriopurbo <satrio_leonheart@yahoo.com>
2024-12-21 02:20:12 +0700

    asd

--- src/module/pt/controller.js
+++ src/module/pt/controller.js
@@ -2,6 +2,8 @@ import { sq ,osbond} from '../../config/connection.js';
 import { Op } from 'sequelize';
 import { tipe } from '../../helper/type.js';
 import { nanoid } from 'nanoid'
+import user from '../user/model.js';
+import { uuid } from 'uuidv4';
 
 
 export class Controller {
@@ -32,6 +34,31 @@ export class Controller {
             res.status(200).json({ status: 200, message: "sukses" ,detail})
         } catch (error) {
             console.log(req.body)
+            console.log(error)
+            res.status(500).json({ status: 500, message: "gagal", data: error })
+        }
+    }
+
+    static async sync_pt(req,res){
+        try {
+            let data = await osbond.query(`SELECT * FROM APPS_LISTTRAINER()`,tipe())
+            data = data.recordset
+            let bulknya=[]
+            data.forEach(el => {        
+                bulknya.push({
+                    id:nanoid(14),
+                    username:el.ID,
+                    nama_user:el.NAMA,
+                    nik:el.NIK,
+                    role:"PT",
+                    kode_club:"SC",
+                    // no_hp_user:el.HP
+                })
+             });
+            await user.bulkCreate(bulknya,{updateOnDuplicate:["nik"]})
+
+            res.status(200).json({ status: 200, message: "sukses" ,data})
+        } catch (error) {
             console.log(error)
             res.status(500).json({ status: 500, message: "gagal", data: error })
         }
--- src/module/pt/route.js
+++ src/module/pt/route.js
@@ -9,6 +9,6 @@ const router = Router()
 
 router.post('/list', Controller.list)
 router.post('/details_by_club_id', Controller.details_by_club_id)
-
+router.post('/sync_pt', Controller.sync_pt)
 
 export default router
