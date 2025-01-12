34b549b493150cb329169f88154623c4a5742b16
satriopurbo <satrio_leonheart@yahoo.com>
2025-01-12 09:20:16 +0700

    asd

--- src/module/referral/route.js
+++ src/module/referral/route.js
@@ -11,7 +11,7 @@ router.post('/update', authentification, Controller.update)
 router.post('/list', Controller.list)
 router.post('/delete', authentification, Controller.delete)
 router.post('/cek_referral', Controller.cek_referral)
-
+router.post('/referral_terbanyak', Controller.referral_terbanyak)
 
 
 export default router
